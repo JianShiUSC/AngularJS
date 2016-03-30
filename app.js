bbd8c173d79d7aae2961afa48da59a7d437c7eb2
Jian Shi <jians@usc.edu>
2016-03-29 23:38:19 -0700

    challenge1

--- app.js
+++ app.js
@@ -2,4 +2,16 @@
 // {
 //     alert('Welcome, Gregg!');
 // }
-var app = angular.module('store', [ ]);
+(function(){
+    var app = angular.module('store', [ ]);
+
+    app.controller('StoreController', function(){           //Notice that controller is attached inside our app
+        this.product = gem;
+    });
+
+    var gem = {
+        name : 'Dodecahedron',
+        price : 2.95,
+        description: '. . .',
+    }
+})();

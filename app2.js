d4e67a9a72261dbee4cf792928864b06cd628332
Jian Shi <jians@usc.edu>
2016-03-30 18:31:38 -0700

    challenge4

---
+++ app2.js
@@ -0,0 +1,83 @@
+(function(){
+    var app = angular.module('store', [ ]);
+
+    app.controller('StoreController', function(){           //Notice that controller is attached inside our app
+        this.products = [
+            {
+                name: 'Awesome Multi-touch Keyboard',
+                price: 250.00,
+                description: "...",
+                images: [
+                    {
+                        full: 'gem-01.gif',
+                        thumb: 'gem-02.gif',
+                    },
+                    {
+                        full: 'gem-03.gif',
+                        thumb: 'gem-04.gif',
+                    }
+                ],
+                reviews: [
+                    {
+                        stars: 5,
+                        body: "I love this product!",
+                        author: "joe@thomas.com"
+                    },
+                    {
+                        stars: 1,
+                        body: "This product sucks",
+                        author: "tim@hater.com"
+                    }
+                ]
+            },
+            {
+                name : 'Pentagonal Gem',
+                price : 5.95,
+                description: ". . .",
+                images: [
+                    {
+                        full: 'gem-05.gif',
+                        thumb: 'gem-06.gif',
+                    },
+                    {
+                        full: 'gem-07.gif',
+                        thumb: 'gem-08.gif',
+                    }
+                ],
+                reviews: [
+                    {
+                        stars: 5,
+                        body: "I love this product!",
+                        author: "joe@thomas.com"
+                    },
+                    {
+                        stars: 1,
+                        body: "This product sucks",
+                        author: "tim@hater.com"
+                    }
+                ]
+            }
+        ];
+    });
+
+    app.controller("PanelController", function(){
+        this.tab = 1;
+        this.selectTab = function(setTab)
+        {
+            this.tab = setTab;
+        };
+        this.isSelected = function(checkTab)
+        {
+            return this.tab === checkTab;
+        };
+    });
+
+    app.controller("ReviewController", function(){
+        this.review = {};
+        this.addReview = function(product){
+            product.reviews.push(this.review);
+            this.review = {};
+        };
+    });
+
+})();
